b2c1baa397bc68491d9e9c3b58ba3025bea5ea0a
Alessandro <alessandrosponte@gmail.com>
2023-12-01 23:36:08 -0300

    Cria versao TypeScript do Ignite Feed

---
+++ 01-fundamentos-reactjs-ts/src/App.tsx
@@ -0,0 +1,59 @@
+import { Post, PostType } from "./components/Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import './global.css';
+import styles from './App.module.css';
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/alessandro-ponte.png',
+      name: 'Alessandro Ponte',
+      role: 'Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-10-30 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/alessandro-ponte.png',
+      name: 'Alessandro Ponte',
+      role: 'Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-10-15 20:00:00'),
+  }
+];
+
+export function App() { 
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                post={post}
+              />
+            )
+          })}          
+        </main>
+      </div>      
+    </div>
+  )
+}
---
+++ 01-fundamentos-reactjs-ts/src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { PencilLine } from '@phosphor-icons/react';
+
+import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
+
+export function Sidebar () {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1603468620905-8de7d86b781e?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            />
+
+            <div className={styles.profile}>            
+                <Avatar src="https://github.com/alessandro-ponte.png" />                
+                <strong>Alessandro Ponte</strong>
+                <span>Salesforce Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
