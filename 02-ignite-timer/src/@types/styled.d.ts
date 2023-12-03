a821d4f582b1d4cad6abec8590de6f583cc06f83
Alessandro <alessandrosponte@gmail.com>
2023-12-02 21:42:46 -0300

    Cria o projeto Ignite Timer

---
+++ 02-ignite-timer/src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+// somente definição de tipos
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
---
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import { Play } from '@phosphor-icons/react'
+import { useForm } from 'react-hook-form'
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Desenvolver uma aplicação web" />
+            <option value="Desenvolver uma aplicação móvel" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount')}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
